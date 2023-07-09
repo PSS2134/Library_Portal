3366b726f45109ae2980354fd6ecd268080103e1
Abhishek Gupta <abhishek24092003gupta@gmail.com>
2023-07-09 22:04:51 +0530

    navbar modified

--- Client/src/components/navbar/navbar.js
+++ Client/src/components/navbar/navbar.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import { BiUserCircle } from "react-icons/bi";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
 function Navbar(props) {
   const Logout = () => {
@@ -11,38 +9,7 @@ function Navbar(props) {
     localStorage.removeItem("useraudify");
     const user = JSON.parse(localStorage.getItem("useraudify"));
 
-function Navbar() {
-    return (
-      <nav className="navbar">
-        <div className="leftbar">
-            <div class="dropdown">
-              <Link to={'/library'}><button class="dropbtn">LIBRARY</button></Link>
-              <div class="dropdown-content">
-                <a href="/library/Fiction">Fiction</a>
-                <a href="/library/Competitive">Competitive</a>
-                <a href="/library/Autobiography">Autobiography</a>
-                <a href="/library/Others">Others</a>
-              </div>
-            </div>
-        </div>
-        <div className="middlebar">
-            <Link to={'/'}><button className="nav-btn">Home</button></Link>
-            <button className="nav-btn">Team</button>
-            {/* <button className="nav-btn">Services</button>
-            <button className="nav-btn">Recommended</button> */}
-            
-        </div>  
-        <div className="rightbar">
-            <Link to={'/user'}><button className="nav-btn user-btn"><BiUserCircle className='user-icon'/></button></Link>
-            <button className="logout-btn">Logout</button>
-        </div> 
-      </nav>
-    )
-  }
-  
-  export default Navbar
     props.updateUser(user);
-  };
   return (
     <nav className="navbar">
       <div className="leftbar">
@@ -62,9 +29,9 @@ function Navbar() {
         <Link to={"/"}>
           <button className="nav-btn">Home</button>
         </Link>
-        <button className="nav-btn">About</button>
-        <button className="nav-btn">Services</button>
-        <button className="nav-btn">Recommended</button>
+        <button className="nav-btn">Team</button>
+        {/* <button className="nav-btn">Services</button>
+            <button className="nav-btn">Recommended</button> */}
       </div>
       <div className="rightbar">
         <Link to={"/user"}>
@@ -72,9 +39,7 @@ function Navbar() {
             <BiUserCircle className="user-icon" />
           </button>
         </Link>
-        <button onClick={Logout} className="logout-btn">
-          Logout
-        </button>
+        <button className="logout-btn" onClick={Logout}>Logout</button>
       </div>
     </nav>
   );
