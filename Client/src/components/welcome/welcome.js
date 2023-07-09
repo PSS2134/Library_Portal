deb89a8f77456bd5f3925665942e464a0a6e94f6
Ashutosh5548 <mishraashutosh5548@gmail.com>
2023-07-09 22:13:57 +0530

    footer-improved,welcoming-section-created

--- Client/src/components/welcome/welcome.js
+++ Client/src/components/welcome/welcome.js
@@ -9,24 +9,25 @@ function Welcome() {
             <div className="paddings innerwidth flexcentre land-container ">
                 <div className=" land-left">
                   <div className="land-title">
-                    <h1>Welcome, To Library_Portal</h1>
+                    <h1>Welcome, To Library!</h1>
                     <div className="land-des">
-                        <div className='flex-btn'>
+                        <p>Your gateway to a world of knowledge and inspiration! Discover a world of knowledge and endless possibilities with our vast collection of books.Whether you're a passionate reader or a curious learner, our library has something for everyone.</p>
+                        {/* <div className='flex-btn'>
                             <h3>New to this app</h3>
                             <Link to={'/signup'}><button className='wel-btn'>Register</button></Link>
-                        </div>
+                        </div> */}
                         <div className='flex-btn'>
-                            <h3>Already Registered</h3>
+                            <Link to={'/signup'}><button className='wel-btn'>Register</button></Link>
                             <Link to={'/login'}><button className='wel-btn'>Login</button></Link>
                         </div>
                     </div>
                   </div>
                 </div>
-                {/* <div className="land-right">
+                <div className="land-right">
                   <div className="image-container">
-                    <img src="https://img.freepik.com/free-vector/movie-theater-signboard-blue_34230-295.jpg?size=338&ext=jpg" alt="" />
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/71wOJkqutUL._AC_SL1240_.jpg" alt="" />
                   </div>
-                </div> */}
+                </div>
             </div>
           </section>
         </div>  
